b869a44eec05078122d7a1cd26ec9c8774613945
enish <toey.arx@gmail.com>
2020-12-30 08:49:54 +0900

    add material-ui settings

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,9 +1,9 @@
 import { GetStaticProps, GetStaticPaths } from 'next'
 
-import { User } from '../../interfaces'
-import { sampleUserData } from '../../utils/sample-data'
-import Layout from '../../components/Layout'
-import ListDetail from '../../components/ListDetail'
+import { User } from 'interfaces'
+import { sampleUserData } from 'utils/sample-data'
+import Layout from 'components/Layout'
+import ListDetail from 'components/ListDetail'
 
 type Props = {
   item?: User
@@ -36,7 +36,7 @@ export default StaticPropsDetail
 
 export const getStaticPaths: GetStaticPaths = async () => {
   // Get the paths we want to pre-render based on users
-  const paths = sampleUserData.map((user) => ({
+  const paths:{params: {id:string }}[] = sampleUserData.map((user) => ({
     params: { id: user.id.toString() },
   }))
 
@@ -51,7 +51,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   try {
     const id = params?.id
-    const item = sampleUserData.find((data) => data.id === Number(id))
+    let item = sampleUserData.find((data) => data.id === Number(id))
+    const temp = params?.temp
+    if (temp) item = {id: 0, name: "temp val is comming"}
     // By returning { props: item }, the StaticPropsDetail component
     // will receive `item` as a prop at build time
     return { props: { item } }
